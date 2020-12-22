c2c7166dc4fa41322a2abcede311f86d427f3dcf
davesolow <davesolow@gmail.com>
2020-12-22 18:01:35 -0500

    fixed random integer issue

--- karger_min_cuts.py
+++ karger_min_cuts.py
@@ -1,45 +1,49 @@
+import random
 from random import randint
 
-v = {}
-e = 0
-with open('data.txt') as file:
-    for index, line in enumerate(file):
-        numbers = [int(number) for number in line.split()]
-        v[numbers[0]] = numbers[1:]
-        e += len(numbers[1:])
+class Karger_Minimum_Cuts:
+    def __init__(self, graph_file):
+        self.v = {}
+        self.e = 0
+        with open(graph_file) as file:
+            for index, line in enumerate(file):
+                numbers = [int(number) for number in line.split()]
+                self.v[numbers[0]] = numbers[1:]
+                self.e += len(numbers[1:])
 
-def random_edge(vertices, total_edges):
-    random_edge = randint(0, total_edges - 1)
-    for vertex, edges in vertices.items():
-        if len(edges) <= random_edge:
-            random_edge -= len(edges)
-        else:
-            from_vertex = vertex
-            to_vertex = edges[random_edge]
-            return from_vertex, to_vertex
+    def random_edge(self):
+        random_edge = randint(0, self.e - 1)
+        for vertex, edges in self.v.items():
+            if len(edges) <= random_edge:
+                random_edge -= len(edges)
+            else:
+                from_vertex = vertex
+                to_vertex = edges[random_edge]
+                return from_vertex, to_vertex
 
-def find_min_cut(vertices, total_edges):
-    min_cut = 0
-    while len(vertices) > 2:
-        vertex_1, vertex_2 = random_edge(vertices, total_edges)
-        total_edges -= len(vertices[vertex_1])
-        total_edges -= len(vertices[vertex_2])
-        vertices[vertex_1].extend(vertices[vertex_2])
-        for vertex in vertices[vertex_2]:
-            vertices[vertex].remove(vertex_2)
-            vertices[vertex].append(vertex_1)
-        vertices[vertex_1] = list(filter(lambda v: v != vertex_1, vertices[vertex_1]))
-        total_edges += len(vertices[vertex_1])
-        vertices.pop(vertex_2)
-    for edges in vertices.values():
-        min_cut = len(edges)
-    return min_cut
+    def find_min_cut(self):
+        min_cut = 0
+        while len(self.v) > 2:
+            vertex_1, vertex_2 = self.random_edge()
+            self.e -= len(self.v[vertex_1])
+            self.e -= len(self.v[vertex_2])
+            self.v[vertex_1].extend(self.v[vertex_2])
+            for vertex in self.v[vertex_2]:
+                self.v[vertex].remove(vertex_2)
+                self.v[vertex].append(vertex_1)
+            self.v[vertex_1] = list(filter(lambda v: v != vertex_1, self.v[vertex_1]))
+            self.e += len(self.v[vertex_1])
+            self.v.pop(vertex_2)
+        for edges in self.v.values():
+            min_cut = len(edges)
+        return min_cut
 
-results = []
-iterations = len(v) * len(v)
-for i in range(iterations):
-    results.append(find_min_cut(v, e))
-
-print(results)
-print(min(results))
-print(find_min_cut(v, e))
+if __name__ == '__main__':
+    iterations = len(Karger_Minimum_Cuts('data.txt').v)
+    minimum_cuts = iterations
+    for i in range(iterations):
+        cutter = Karger_Minimum_Cuts('data.txt')
+        cut = cutter.find_min_cut()
+        if cut < minimum_cuts:
+            minimum_cuts = cut
+    print(minimum_cuts)
