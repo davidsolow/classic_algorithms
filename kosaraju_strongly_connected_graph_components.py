d5f4340063a27f17f713fcb040afe7a39b22abad
davesolow <davesolow@gmail.com>
2021-01-08 20:33:25 -0500

    Toposort working, need to fix how dictionary is orderd/interated through during Kosaraju. Currently using a lambda and sorted function, which returns a list of tuples and breaks when trying to call the dictionary later.

--- kosaraju_strongly_connected_graph_components.py
+++ kosaraju_strongly_connected_graph_components.py
@@ -3,6 +3,11 @@ class Kosaraju:
     def __init__(self, graph_file): #object instantiated based on a text file with one column of 'from' vertices and a second colume of 'to' vertices separated by new lines
         self.graph = {} #our normal graph
         self.graph_r = {}   #reversed graph for initial DFS search
+        self.topoOrder = {}
+        self.exploredTopo = []
+        self.curLabel = len(self.graph)
+        self.scc_number = 0
+        self.scc = {}
         with open(graph_file) as file:
             for index, line in enumerate(file):
                 key = int(line.split()[0])  #dictionary key for adjacency list (from vertex is always a key)
@@ -16,32 +21,49 @@ class Kosaraju:
                 else:
                     self.graph_r[value].append(key)
 
+#Kosaraju
+def kosaraju(self):
+    order = topo_sort(self)
+    self.exploredTopo = []
+    self.scc_number = 0
+    self.scc = {}
+    for v in sorted(order.items(), key=lambda x: x[1]):
+        if v not in self.exploredTopo:
+            self.scc_number += 1
+            self.exploredTopo, self.scc = dfs_scc(self, v)
+    return self.scc_number, self.scc
+
 #Topological sort helper function to find order of reversed graph
 def topo_sort(self):
-    topoOrder = {}
-    exploredTopo = []
-    curLabel = len(self.graph_r)
+    self.topoOrder = {}
+    self.exploredTopo = []
+    self.curLabel = len(self.graph)
     for v in self.graph_r:
-        if v not in exploredTopo:
-            exploredTopo += dfs_topo(self, v, exploredTopo, curLabel, topoOrder)[0]
-            topoOrder.update(dfs_topo(self, v, exploredTopo, curLabel, topoOrder)[2])
-            curLabel = dfs_topo(self, v, exploredTopo, curLabel, topoOrder)[1]
-    return topoOrder, exploredTopo, curLabel
+        if v not in self.exploredTopo:
+            self.exploredTopo, self.topoOrder, self.curLabel = dfs_topo(self, v)
+    return self.topoOrder
 
 #DFS Topo helper function to search graph and apply order numbers ie f(v) values
-def dfs_topo(self, s, exploredTopo, curLabel, topoOrder):
-    exploredTopo.append(s)
+def dfs_topo(self, s):
+    self.exploredTopo.append(s)
     for v in self.graph_r[s]:
-        if v not in exploredTopo:
-            dfs_topo(self, v, exploredTopo, curLabel, topoOrder)
-    topoOrder[s] = curLabel
-    curLabel -= 1
-    return exploredTopo, curLabel, topoOrder
+        if v not in self.exploredTopo:
+            dfs_topo(self, v)
+    self.topoOrder[s] = self.curLabel
+    self.curLabel -= 1
+    return self.exploredTopo, self.topoOrder, self.curLabel
 
-#Depth-First-Search with SCC label Helper Function
-
+#DFS scc helper function to go through normal graph and accrue SCCs
+def dfs_scc(self, s):
+    self.exploredTopo.append(s)
+    self.scc[s] = self.scc_number
+    for v in self.graph[s]:
+        if v not in self.exploredTopo:
+            dfs_scc(self, v)
+    return self.exploredTopo, self.scc
 
 #Instantiate an object for data file and run Kosaraju on it
 if __name__ == '__main__':
     graph_object = Kosaraju('test_data.txt')
-    print(topo_sort(graph_object))
+    order1 = topo_sort(graph_object)
+    print(sorted(order1.items(), key=lambda x: x[1]))
