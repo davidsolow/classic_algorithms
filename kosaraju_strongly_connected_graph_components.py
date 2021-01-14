a526586451d1296cd7ab8c0b06f289fc9a361b08
davesolow <davesolow@gmail.com>
2021-01-13 19:37:18 -0500

    Done! Runtime brought down to ~13 seconds for 800K+ vertices! Need to add comments push final.

--- kosaraju_strongly_connected_graph_components.py
+++ kosaraju_strongly_connected_graph_components.py
@@ -1,5 +1,5 @@
 #Import libraries
-import sys, threading
+import sys, threading, datetime
 sys.setrecursionlimit(800000)
 threading.stack_size(67108864)
 
@@ -9,6 +9,8 @@ class Kosaraju:
         self.graph = {} #our normal graph
         self.graph_r = {}   #reversed graph for initial DFS search
         self.topoOrder = {}
+        for i in range(1, number_of_vertices+1):
+            self.topoOrder[i] = 0
         self.number_of_vertices = number_of_vertices
         self.exploredTopo = {}
         for i in range(1, number_of_vertices+1):
@@ -40,7 +42,7 @@ def kosaraju_scc(self):
     self.exploredTopo = dict.fromkeys(self.exploredTopo, False)
     self.scc_number = 0
     self.scc = {}
-    for v in order:
+    for v in order.values():
         if self.exploredTopo[v] == False:
             self.scc_number += 1
             self.exploredTopo, self.scc = dfs_scc(self, v)
@@ -50,10 +52,8 @@ def kosaraju_scc(self):
         top5[i] = 0
     while len(top5) < 5:
         top5[len(top5)+1] = 0
-    for j in self.scc:
-        for k in top5.keys():
-            if self.scc[j] == k:
-                top5[k] = top5[k] + 1
+    for j in self.scc.values():
+        top5[j] += 1
     top5 = {k: v for k, v in sorted(top5.items(), key=lambda item: item[1])}
     top5 = list(top5.values())
     top5 = [top5[-1], top5[-2], top5[-3], top5[-4], top5[-5]]
@@ -61,13 +61,12 @@ def kosaraju_scc(self):
 
 #Topological sort helper function to find order of reversed graph
 def topo_sort(self):
-    self.topoOrder = {}
+    self.topoOrder = dict.fromkeys(self.topoOrder, 0)
     self.exploredTopo = dict.fromkeys(self.exploredTopo, False)
     self.curLabel = self.number_of_vertices
     for v in self.graph_r:
         if self.exploredTopo[v] == False:
             self.exploredTopo, self.topoOrder, self.curLabel = dfs_topo(self, v)
-    self.topoOrder = {k: v for k, v in sorted(self.topoOrder.items(), key=lambda item: item[1])}
     return self.topoOrder
 
 #DFS Topo helper function to search graph and apply order numbers ie f(v) values
@@ -76,7 +75,7 @@ def dfs_topo(self, s):
     for v in self.graph_r[s]:
         if self.exploredTopo[v] == False:
             dfs_topo(self, v)
-    self.topoOrder[s] = self.curLabel
+    self.topoOrder[self.curLabel] = s
     self.curLabel -= 1
     return self.exploredTopo, self.topoOrder, self.curLabel
 
@@ -92,7 +91,11 @@ def dfs_scc(self, s):
 #Instantiate an object for data file and run Kosaraju on it
 def main():
     if __name__ == '__main__':
-        graph_object = Kosaraju('test_data.txt', 12)
+        started = datetime.datetime.now()
+        graph_object = Kosaraju('data.txt', 875714)
         print(kosaraju_scc(graph_object))
+        ended = datetime.datetime.now()
+        runtime = ended - started
+        print("Runtime: ", runtime)
 thread = threading.Thread(target=main)
 thread.start()
