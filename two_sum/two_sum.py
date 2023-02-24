573256098c793f071cf8d860213f726bb40a5896
David Solow <davidsolow@pop-os.localdomain>
2023-02-24 15:42:30 -0500

    Finished

--- two_sum/two_sum.py
+++ two_sum/two_sum.py
@@ -1,12 +1,33 @@
+import datetime
 class two_sum:
-    def __init__(self, data):
-        self.values = {}
-        with open(data,m 'r') as f:
-            lines = f.readlines()
-            for line in lines:
-                self.values[int(line)] = 0
-        self.number_of_two_sums = two_sum.solve(self.values)
+    def __init__(self, data, range_start, range_end):
+        self.targets = {}
+        self.values = set()
+        for i in range(range_start, range_end+1):
+            self.targets[i] = False
+        with open(data, 'r') as f:
+            for line in f:
+                self.values.add(int(line))
+        self.targets = two_sum.solve(self)
+        self.number_of_two_sums = sum(1 for value in self.targets.values() if value)
 
-    def solve(values):
-        for t, i in range(-10000, 10001), values.values:
-            y = t - i
+    def two_sum(self, target):
+        for x in self.values:
+            y = target - x
+            if y in self.values and y != x:
+                return True
+        return False
+    
+    def solve(self):
+        for t in self.targets.keys():
+            if two_sum.two_sum(self, t) == True:
+                self.targets[t] = True
+        return self.targets
+
+if __name__ == '__main__':
+    started = datetime.datetime.now()
+    object = two_sum('data.txt', -10000, 10000)
+    print(object.number_of_two_sums)
+    ended = datetime.datetime.now()
+    runtime = ended - started
+    print("Runtime: ", runtime)
