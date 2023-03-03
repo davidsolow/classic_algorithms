37d7f12669e5c2aabaeb4adbf8270eedb3b554a8
David Solow <davesolow@gmail.com>
2023-03-03 10:18:35 -0500

    Simplify

--- huffman/huffman.py
+++ huffman/huffman.py
@@ -13,16 +13,19 @@ class Huffman:
         self.tree = self.huffman(forest)
 
     def huffman(self, forest):
-        while len(forest) >= 2:
+        counter = 0
+        while counter <= 5: #len(forest) >= 2:
             t_one = forest.pop(forest.index(min(forest, key = sum)))
             t_two = forest.pop(forest.index(min(forest, key = sum)))
             t_three = Huffman.merge_trees(t_one, t_two)
             forest.append(t_three)
+            counter += 1
         return forest
     
     def merge_trees(t_one, t_two):
-        new_tree = [0]
-        max_level = max(len(t_one) - 1, len(t_two) - 1, key = Huffman.get_level)
+        max_level = max(len(t_one) - 1, len(t_two) - 1, key = Huffman.get_level) + 1
+        n_items = (2^(max_level)) - 1
+        new_tree = [0] * n_items
         for i in range(max_level + 1):
             for j in t_one:
                 if Huffman.get_level(t_one.index(j)) == i:
@@ -30,6 +33,10 @@ class Huffman:
             for k in t_two:
                 if Huffman.get_level(t_two.index(k)) == i:
                     new_tree.append(k)
+        
+        for i in range(1, len(new_tree)):
+            for
+            
         return new_tree
 
     def get_level(index):
