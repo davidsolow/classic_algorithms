54c91dab2fc10c7d070d14dce7c830458fbddfdc
David Solow <davesolow@gmail.com>
2023-03-03 21:36:36 -0500

    Updated Huffman and MWIS

--- huffman/huffman.py
+++ huffman/huffman.py
@@ -1,51 +1,33 @@
 import datetime
 import math
+import heapq
 
 class Huffman:
     def __init__(self, data):
         forest = []
+        tracker = {}
         with open(data, 'r') as f:
             lines = f.readlines()
             for line in lines[1:]:
-                item = line.split()
-                item = [int(i) for i in item]
-                forest.append(item)
-        self.tree = self.huffman(forest)
+                forest.append((int(line), [int(line)]))
+                tracker[int(line)] = 0
+        heapq.heapify(forest)
+        self.tree = self.huffman(forest, tracker)
 
-    def huffman(self, forest):
-        counter = 0
-        while counter <= 5: #len(forest) >= 2:
-            t_one = forest.pop(forest.index(min(forest, key = sum)))
-            t_two = forest.pop(forest.index(min(forest, key = sum)))
-            t_three = Huffman.merge_trees(t_one, t_two)
-            forest.append(t_three)
-            counter += 1
-        return forest
-    
-    def merge_trees(t_one, t_two):
-        max_level = max(len(t_one) - 1, len(t_two) - 1, key = Huffman.get_level) + 1
-        n_items = (2^(max_level)) - 1
-        new_tree = [0] * n_items
-        for i in range(max_level + 1):
-            for j in t_one:
-                if Huffman.get_level(t_one.index(j)) == i:
-                    new_tree.append(j)
-            for k in t_two:
-                if Huffman.get_level(t_two.index(k)) == i:
-                    new_tree.append(k)
-        
-        for i in range(1, len(new_tree)):
-            for
-            
-        return new_tree
-
-    def get_level(index):
-        return math.floor(math.log2(index + 1))
+    def huffman(self, forest, tracker):
+        while len(forest) >= 2:
+            node1 = heapq.heappop(forest)
+            node2 = heapq.heappop(forest)
+            node3 = (node1[0] + node2[0], node1[1] + node2[1])
+            heapq.heappush(forest, node3)
+            for i in node3[1]:
+                tracker[i] = tracker[i] + 1
+        return forest, tracker, max(tracker.values()), min(tracker.values())
 
 if __name__ == '__main__':
     started = datetime.datetime.now()
-    object = Huffman('test.txt')
-    print(object.tree)
+    object = Huffman('data.txt')
+    print(object.tree[2:])
     ended = datetime.datetime.now()
     runtime = ended - started
     print("Runtime: ", runtime)
